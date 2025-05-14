8d44ae3b1f914f092a6689891e7d2d93089a6001
Pierre Orellana <89483065+pierorellana@users.noreply.github.com>
2025-05-13 23:26:52 -0500

    Create deliver.bat

---
+++ jenkins/scripts/deliver.bat
@@ -0,0 +1,17 @@
+@echo off
+echo The following Maven command installs your Maven-built Java application
+echo into the local Maven repository, which will ultimately be stored in
+echo Jenkins's local Maven repository.
+call mvn jar:jar install:install help:evaluate -Dexpression=project.name
+
+echo The following command extracts the value of the <name/> element
+echo within <project/> of your Java/Maven project's "pom.xml" file.
+for /f "tokens=*" %%G in ('mvn -q -DforceStdout help:evaluate -Dexpression^=project.name') do set NAME=%%G
+
+echo The following command behaves similarly to the previous one but
+echo extracts the value of the <version/> element within <project/> instead.
+for /f "tokens=*" %%G in ('mvn -q -DforceStdout help:evaluate -Dexpression^=project.version') do set VERSION=%%G
+
+echo The following command runs and outputs the execution of your Java
+echo application (which Jenkins built using Maven) to the Jenkins UI.
+java -jar target\%NAME%-%VERSION%.jar
